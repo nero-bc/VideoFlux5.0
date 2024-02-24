69b253c22e4423c2e391420c0f0b003c225dbfa5
nero-bc <126649764+nero-bc@users.noreply.github.com>
2024-02-24 17:17:26 +0530

    Update start.py

--- bot/start.py
+++ bot/start.py
@@ -717,8 +717,8 @@ async def _speed_test(event):
 async def _startmsg(event):
     text = f"Hi {get_mention(event)}, I Am Alive."
     await event.reply(text, buttons=[
-    [Button.url('⭐ Bot By 𝚂𝚊𝚑𝚒𝚕 ⭐', 'https://t.me/nik66')],
-    [Button.url('❤ Join Channel ❤', 'https://t.me/nik66x')]
+    [Button.url('🌺 Bot Owner', 'https://t.me/StupidBoi69')],
+    [Button.url('🌸 Bot Channel', 'https://t.me/jr_bots')]
 ])
     return
 
